fd265b5b5772d3a409f32794d3141054d6ae6f00
Rahul Sharma <rahulsharmab94@gmail.com>
2025-12-17 09:13:28 +0000

    while clicking here date of my choice is not selecting (_for element src

--- src/components/transactions/add-transaction-form.tsx
+++ src/components/transactions/add-transaction-form.tsx
@@ -339,7 +339,7 @@ export default function AddTransactionForm({ onAddTransaction, children }: AddTr
                             selected={field.value}
                             onSelect={field.onChange}
                             disabled={(date) =>
-                              date > new Date() || date < new Date("1900-01-01")
+                              date > new Date()
                             }
                             initialFocus
                           />
