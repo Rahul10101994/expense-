dde9c0b721ab5ed4b1517885a2e3c3fae0b5f627
Rahul Sharma <rahulsharmab94@gmail.com>
2026-01-01 11:21:04 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -150,10 +150,8 @@ export default function LoginPage() {
                   <FormItem>
                     <div className="flex justify-between items-center">
                       <FormLabel>Password</FormLabel>
-                      <Link href="/forgot-password" passHref legacyBehavior>
-                        <a className="text-xs text-primary hover:underline cursor-pointer">
+                      <Link href="/forgot-password" className="text-xs text-primary hover:underline cursor-pointer">
                           Forgot Password?
-                        </a>
                       </Link>
                     </div>
                     <FormControl>
